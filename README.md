a0a4e12804b27ec11c0fc5edb0e38f0799bb6c8d
funnythingz <funnythingz69+github@gmail.com>
2017-05-19 15:32:50 +0900

    use Sprockets

--- README.md
+++ README.md
@@ -6,29 +6,6 @@
 $ bundle install
 ```
 
-## tree
-
-```
-.
-├── Gemfile
-├── Gemfile.lock
-├── README.md
-├── Rakefile
-├── app.rb
-├── assets
-│   └── sass
-│       ├── bourbon
-│       ├── modules
-│       └── styles.sass
-├── config.ru
-├── public
-│   ├── styles.css
-│   └── styles.css.map
-└── views
-    ├── app.slim
-    └── home.slim
-```
-
 ## run
 
 development
@@ -44,25 +21,3 @@ $ RACK_ENV=production rackup -p 8888
 ```
 
 access to http://localhost:8888
-
-## assets
-
-assets:compile
-
-```
-$ rake assets:compile
-```
-
-## sass
-
-sass:compile
-
-```
-$ rake sass:compile
-```
-
-sass:watch
-
-```
-$ rake sass:watch
-```
--- app.rb
+++ app.rb
@@ -1,10 +1,30 @@
 require 'sinatra/base'
 require 'sinatra/reloader'
+require 'sprockets'
+require 'sprockets-helpers'
 
 class App < Sinatra::Base
 
+  set :assets, Sprockets::Environment.new(root)
+
+  configure do
+    assets.append_path File.join(root, 'assets', 'css')
+    assets.append_path File.join(root, 'assets', 'js')
+    assets.append_path File.join(root, 'assets', 'images')
+
+    Sprockets::Helpers.configure do |config|
+      config.environment = assets
+      config.prefix      = '/assets'
+      config.digest      = true
+    end
+  end
+
   configure :development do
     register Sinatra::Reloader
+  end
+
+  helpers do
+    include Sprockets::Helpers
   end
 
   get '/' do
