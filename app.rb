1987e93a0436e46ca1ae50bb9daf3cb52916dc00
funnythingz <funnythingz69+github@gmail.com>
2017-06-21 00:54:13 +0900

    precompile

--- app.rb
+++ app.rb
@@ -4,6 +4,8 @@ Bundler.require
 class App < Sinatra::Base
 
   set :assets, Sprockets::Environment.new(root)
+  set :assets_precompile, %w(app.js styles.sass *.png *.jpg *.svg *.eot *.ttf *.woff)
+  set :manifest, Sprockets::Manifest.new(assets, 'public/assets/manifset.json')
 
   configure do
     assets.append_path File.join(root, 'assets', 'css')
@@ -14,9 +16,12 @@ class App < Sinatra::Base
     Sprockets::Helpers.configure do |config|
       config.environment = assets
       config.prefix      = '/assets'
-      config.manifest    = Sprockets::Manifest.new(assets, 'manifset.json')
+      config.manifest    = manifest
       config.digest      = true
       config.public_path = '/public'
+
+      #TODO: precompileの仕組みをつくる
+      manifest.compile(assets_precompile)
     end
   end
 
